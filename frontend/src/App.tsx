aaf5d71504f70982fd01f6ead4cd48793dbdfc27
Tayyib <toyeebyunus484@gmail.com>
2026-09-03 22:19:50 +0100

    Restore responsive mobile landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@ import PhotographyLanding from "./PhotographyLanding";
 import LoadingPreview from "./LoadingPreview";
 import OperatorPanel from "./OperatorPanel";
 import UserApp from "./UserApp";
-import LandingScaleFrame from "./LandingScaleFrame";
 
 export default function App() {
   if (window.location.pathname === "/loading-preview") return <LoadingPreview />;
   if (window.location.pathname === "/operator") return <OperatorPanel />;
-  return window.location.pathname === "/app" ? <UserApp /> : <LandingScaleFrame><PhotographyLanding /></LandingScaleFrame>;
+  return window.location.pathname === "/app" ? <UserApp /> : <PhotographyLanding />;
 }
